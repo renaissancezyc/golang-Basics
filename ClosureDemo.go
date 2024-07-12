afefdf1cba1a3cd525ecc47bbe5f6f5a87f268e4
Ren <renaissancezyc@gmail.com>
2024-07-12 13:33:38 +0800

    add ClosureDemo.go

---
+++ ClosureDemo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+)
+
+func a() func() int {
+    i := 0
+    b := func() int {
+        i++
+        fmt.Println(i)
+        return i
+    }
+    return b
+}
+
+func main() {
+    c := a()
+    c()
+    c()
+    c()
+
+    a() //不会输出i
+}
+
+// 输出 1 2 3
